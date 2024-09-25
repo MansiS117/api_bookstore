0fc30ca553a131386cad58761452891141e7114b
MansiS117 <msongirkar@bestpeers.com>
2024-09-25 16:20:23 +0530

    added url for crud in books

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 # urls.py
 from django.urls import path,include
-from .views import BookListView, BookDetailView, CategoryDetailView, CategoryListView, UserRegistrationView, LoginView, LogoutView
+from .views import BookListView, BookDetailView, CategoryDetailView, CategoryListView, UserRegistrationView, LoginView, LogoutView, BookCreateView, BookUpdateView, BookDeleteView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -12,6 +14,11 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', LoginView.as_view(), name='user-login'), 
     path('logout/', LogoutView.as_view(), name='user-logout'), 
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
