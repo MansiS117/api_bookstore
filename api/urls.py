3ceb5edce133d5b37f9d1f0a5f57630559404de2
MansiS117 <msongirkar@bestpeers.com>
2024-09-26 19:20:54 +0530

    trial cart views

--- api/urls.py
+++ api/urls.py
@@ -1,23 +1,37 @@
 # urls.py
-from django.urls import path,include
-from .views import BookListView, BookDetailView, CategoryDetailView, CategoryListView, UserRegistrationView, LoginView, LogoutView, BookCreateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include, path
 
-
+from .views import (
+    BookCreateView,
+    BookDetailView,
+    BookListView,
+    CategoryDetailView,
+    CategoryListView,
+    LoginView,
+    LogoutView,
+    UserRegistrationView,
+    CartListView,
+)
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('categories/', CategoryListView.as_view(), name='category-list'),
-    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
-    path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('login/', LoginView.as_view(), name='user-login'), 
-    path('logout/', LogoutView.as_view(), name='user-logout'), 
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
- 
-
+    path("books/", BookListView.as_view(), name="book-list"),
+    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
+    path("categories/", CategoryListView.as_view(), name="category-list"),
+    path(
+        "categories/<int:pk>/",
+        CategoryDetailView.as_view(),
+        name="category-detail",
+    ),
+    path("register/", UserRegistrationView.as_view(), name="user-register"),
+    path("login/", LoginView.as_view(), name="user-login"),
+    path("logout/", LogoutView.as_view(), name="user-logout"),
+    path("books/create/", BookCreateView.as_view(), name="book-create"),
+    path("cart/", CartListView.as_view(), name="cart"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
--- api/views.py
+++ api/views.py
@@ -1,31 +1,43 @@
-from .serializer import BookDetailSerializer,BookListSerializer, CategoryListSerializer,CategoryDetailSerializer, UserRegistrationSerializer, LoginSerializer
+from .serializer import (
+    BookDetailSerializer,
+    BookListSerializer,
+    CategoryListSerializer,
+    CategoryDetailSerializer,
+    UserRegistrationSerializer,
+    LoginSerializer,
+    CartSerializer,
+    CartItemSerializer,
+)
 from rest_framework.response import Response
 from rest_framework import generics, status
-from .models import Book, Category
+from .models import Book, Category, Cart, CartItem
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate, login
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
-from . permissions import IsSeller
-
+from .permissions import IsSeller, IsBuyer
 
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookListSerializer
 
+
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookDetailSerializer
 
-class CategoryListView(generics.ListAPIView):   
+
+class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoryListSerializer
 
+
 class CategoryDetailView(generics.RetrieveAPIView):
-    queryset = Category.objects.all()   
+    queryset = Category.objects.all()
     serializer_class = CategoryDetailSerializer
+
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
@@ -34,29 +46,34 @@ class UserRegistrationView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({
-            "message": "Registration successful!", 
-            "user": {
-                "email": user.email,
-                "user_type": user.user_type
-            }
-        }, status=status.HTTP_201_CREATED)
-    
+        return Response(
+            {
+                "message": "Registration successful!",
+                "user": {"email": user.email, "user_type": user.user_type},
+            },
+            status=status.HTTP_201_CREATED,
+        )
+
 
 class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.validated_data['user']  # Get the authenticated user
-            token, created = Token.objects.get_or_create(user=user)  # Create or retrieve token
-            
-            return Response({
-                "token": str(token),
-                "message": "Logged in successfully."
-            }, status=status.HTTP_200_OK)
+            user = serializer.validated_data[
+                "user"
+            ]  # Get the authenticated user
+            token, created = Token.objects.get_or_create(
+                user=user
+            )  # Create or retrieve token
+
+            return Response(
+                {"token": str(token), "message": "Logged in successfully."},
+                status=status.HTTP_200_OK,
+            )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]  # Ensure the user is authenticated
 
@@ -68,19 +85,30 @@ class LogoutView(APIView):
             # Delete the token to log out the user
             token.delete()
 
-            return Response({
-                "message": "Logged out successfully."
-            }, status=status.HTTP_200_OK)
+            return Response(
+                {"message": "Logged out successfully."},
+                status=status.HTTP_200_OK,
+            )
 
-        return Response({
-            "error": "No active session found."
-        }, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response(
+            {"error": "No active session found."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookDetailSerializer
     permission_classes = [IsSeller]
 
     def perform_create(self, serializer):
-        serializer.save(seller=self.request.user) 
-  
+        serializer.save(seller=self.request.user)
+
+
+class CartListView(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [IsBuyer]
+
+    def perform_create(self, serializer):
+        serializer.save(buyer=self.request.user)
